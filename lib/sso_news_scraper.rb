3bf3fd7aaf55a812460d245ee625530bf5ca7bab
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-23 18:34:22 -0700

    updating cli class

--- lib/sso_news_scraper.rb
+++ lib/sso_news_scraper.rb
@@ -1,8 +1,9 @@
-require "sso_news_scraper/version"
+require_relative "sso_news_scraper/version"
+require_relative "sso_news_scraper/cli"
 
 module SsoNewsScraper
   class Error < StandardError; end
   # Your code goes here...
 end
 
-puts "I did it!"
+
---
+++ lib/sso_news_scraper/cli.rb
@@ -0,0 +1,9 @@
+class CLI
+  def run
+    greeting
+  end
+
+  def greeting 
+    puts "Welcome to SSO News Scraper"
+  end
+end
