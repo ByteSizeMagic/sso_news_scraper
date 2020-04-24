2971f58c2ffde38c48a0e72793efe54cd61fa6d5
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-24 00:37:19 -0700

    updated cli class

--- lib/sso_news_scraper/cli.rb
+++ lib/sso_news_scraper/cli.rb
@@ -1,9 +1,29 @@
 class CLI
   def run
+    system("clear")
     greeting
+
+
+  end_program
   end
 
   def greeting 
-    puts "Welcome to SSO News Scraper"
+    puts "Welcome to Star Stable Online News Scraper!"
   end
-end
+
+  # while menu != "exit"
+
+  # end
+
+  def menu 
+    puts "Please choose an option: "
+    list_options
+    user_input = gets.strip.downcase
+    return input
+  end
+  
+  def end_program
+    puts "Thanks for checking out the news for Star Stable Online, see ya soon!"
+  end
+
+  end
