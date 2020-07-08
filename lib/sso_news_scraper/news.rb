3e5e8cebb8a334230cd6e5a3a490dac552cee40e
ByteSizeMagic <bytesizemagic@gmail.com>
2020-07-08 12:56:11 -0700

    updated scraper class

--- lib/sso_news_scraper/news.rb
+++ lib/sso_news_scraper/news.rb
@@ -1,11 +1,19 @@
 class SsoNewsScraper::News
+  attr_accessor :title, :updated, :lead, :more
+
   def self.all
 
   end
   
   def self.scrape_articles
     ## ./bin/sso_news_scraper
-    ## Nokogiri::HTML(open('https://www.starstable.com/en/#news'))
-    binding.pry
+    doc = Nokogiri::HTML(open('https://www.starstable.com/news'))
+    doc.css('.article').each do |article_title|
+        @title = article_title.css('.article-text h2').text
+        @updated = article_title.css('.article-text .updated').text.delete("\n")
+        @lead = article_title.css('.article-text .lead').text.delete("\n")
+        @more = "www.starstable.com/" + article_title.css("a").first["href"]
+      binding.pry
+    end
   end
 end
